a954e87dbf86c92bc4f060d25b5c42fd94da5e4c
Martin Buchleitner <mbuchleitner@infralovers.com>
2019-12-01 10:10:37 +0100

    v3 with ini file configuration of port + output
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 # Use an official Python runtime as a parent image
 FROM python:3.7-slim
 
+ENV NAME World
+
 # Set the working directory to /app
 WORKDIR /app
 
--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from flask import Flask
 import os
 import socket
 import datetime
+import configparser
 
 app = Flask(__name__)
+configParser = configparser.ConfigParser()
 
 @app.route("/")
 def hello():
@@ -11,10 +13,17 @@ def hello():
     now = datetime.datetime.now()
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Domainname: </b> {domainname}<br/>" \
+           "<b>Port: </b> {port}<br/>" \
+           "<b>Config Sections: </b> {sections}<br/>" \
            "<b>Current Date:</b> {currentdate}<br/>"
     return html.format(name=os.getenv("NAME", "world"),
                        hostname=socket.gethostname(),
+                       domainname=configParser.get('app', 'domainname'),
+                       port=configParser.get('app','port'),
+                       sections=', '.join(configParser.sections()),
                        currentdate=str(now))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    configParser.read('/app/etc/config.ini')
+    app.run(host='0.0.0.0', port=configParser.get('app','port'))
